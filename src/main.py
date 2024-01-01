58a5111bf4f7a50738e5a0984ce4fec4671d76e0
Daniel <macpro@Daniels-MacBook-Pro.local>
2024-01-01 21:16:56 +0000

    Fixed path references

--- src/main.py
+++ src/main.py
@@ -4,38 +4,54 @@ import sqlalchemy as db
 from sqlalchemy import text
 import os
 
+
 def write_to_DB():
-    
-    REFERENCED = ['departments', 'employees', 'titles']  # A list of referenced tables.
-    REFERENCING = ['salaries', 'dept_emp', 'dept_manager'] # A list of referencing tables.
-    
-    cnx = db.create_engine("postgresql+psycopg2://postgres:postgres@postgres/company")    
-    conn = cnx.connect()   
-    
+    REFERENCED = ["departments", "employees", "titles"]  # A list of referenced tables.
+    REFERENCING = [
+        "salaries",
+        "dept_emp",
+        "dept_manager",
+    ]  # A list of referencing tables.
+
+    cnx = db.create_engine("postgresql+psycopg2://postgres:postgres@postgres/company")
+    conn = cnx.connect()
+
     # Write the referenced tables data to the database.
     for table in REFERENCED:
-        data = pd.read_csv(f"../data/{table}.csv")
-        data.to_sql(table, con=conn, if_exists='replace', index=False)
+        data = pd.read_csv(os.path.join(os.getcwd(), "data", f"{table}.csv"))
+        data.to_sql(table, con=conn, if_exists="replace", index=False)
         print(f"Written {table} data to the database.")
-    
-    # Write the referencing tables data to the database. 
+
+    # Write the referencing tables data to the database.
     for table in REFERENCING:
-        data = pd.read_csv(f"../data/{table}.csv")
-        data.to_sql(table, con=conn, if_exists='replace', index=False)
+        data = pd.read_csv(os.path.join(os.getcwd(), "data", f"{table}.csv"))
+        data.to_sql(table, con=conn, if_exists="replace", index=False)
         print(f"Written {table} data to the database.")
-        
+
     conn.close()
 
 
-def import_DB_schema():      
-    cnx = psycopg2.connect(database="company", user='postgres', password='postgres', host='postgres', port='5432')    
+def import_DB_schema():
+    cnx = psycopg2.connect(
+        database="company",
+        user="postgres",
+        password="postgres",
+        host="postgres",
+        port="5432",
+    )
     conn = cnx.cursor()
 
-    conn.execute(open("../queries/create_queries.sql", 'r').read())
+    conn.execute(
+        open(
+            os.path.join(os.getcwd(), "queries", "create_queries.sql"),
+            "r",
+        ).read()
+    )
     print(f"Imported schema to the database.")
 
     conn.close()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     import_DB_schema()
     write_to_DB()
